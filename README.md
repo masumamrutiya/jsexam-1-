027b2370d3236e9028a308e81b3add25b04a45a0
masumamrutiya <masumamrutiya29@gmail.com>
2024-02-19 11:34:37 +0530

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jsexam-1-
+![alt text](<Screenshot 2024-02-19 112649.png>)
+
+![alt text](<Screenshot 2024-02-19 112624-1.png>)
---
+++ index.js
@@ -0,0 +1,24 @@
+
+// **********question-1***********
+
+var units=200;
+var unit=3;
+var fixed=50;
+var m=units*unit+fixed;
+var tb=m*10/100;
+var s=m-tb;
+console.log(s);
+
+//**********question-2***********
+
+// var ctc=1200000;
+// var month=ctc/12;
+// var home=month*0.2;
+// var k=month-home;-
+// var traveller=k*0.05;
+// var m=k-traveller;
+// console.log(m);
+
+
+
+
